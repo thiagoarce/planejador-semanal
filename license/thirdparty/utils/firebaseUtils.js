98505490a3c8ab68f3dbcb616a9d9a422acf5e95
thiagoarce <thiagoarce@outlook.com>
2023-05-07 18:25:48 -0400

    Firebase initted

---
+++ license/thirdparty/utils/firebaseUtils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiDsWpz0J_X4rPsQYUJcUyLVazRtiA8dk",
+  authDomain: "planejador-semanal.firebaseapp.com",
+  projectId: "planejador-semanal",
+  storageBucket: "planejador-semanal.appspot.com",
+  messagingSenderId: "1059522956155",
+  appId: "1:1059522956155:web:02bc9030ff589be44b8bde"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
